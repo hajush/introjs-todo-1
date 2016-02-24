10d2ca60616220425c059786b92847e44e97ba9a
fresh5447 <doug@kosmojo.com>
2016-02-24 09:10:32 -0700

    hook up react to render simple component

--- public/app.js
+++ public/app.js
@@ -1,1 +1,21 @@
-console.log('hello from app js')
+var Todos = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <h2> My list of todos </h2>
+      </div>
+      )
+  }
+});
+
+var App = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <Todos/>
+      </div>
+      )
+  }
+});
+
+React.render(<App/>, document.getElementById('app'));
