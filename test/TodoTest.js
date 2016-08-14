3a399baf37f1c11abb39389d0ab5f037e2e0370b
hajush <harold@shinsato.com>
2016-08-13 22:10:28 -0600

    mocha chai imports and a simple test available to TravisCI

---
+++ test/TodoTest.js
@@ -0,0 +1,11 @@
+var assert = require('chai').assert;
+
+describe("Todo", function() {
+  it("can test", function() {
+    //Arrange
+    //Act
+    //Assert
+    //assert.equal(actual, expected);
+  });
+
+});
